d11e111d3680159ce5d786e83a0e6d688f8832f6
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-12 11:59:57 +0430

    Initial Setup #1

---
+++ src/main/java/af/asr/pdfgenerator/PhoenixPdfgeneratorApplication.java
@@ -0,0 +1,13 @@
+package af.asr.pdfgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixPdfgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixPdfgeneratorApplication.class, args);
+	}
+
+}
